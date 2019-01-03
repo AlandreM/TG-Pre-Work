c21d485bfd0090602dc73ecc3eb72753c70e452b
AM20 <alandrem@gmail.com>
2019-01-03 15:15:01 -0500

    There are other objects besides console that can be used. In this exercise we used the Math and Number objects to generate random numbers (Math.floor(Math.random()), make them whole (Math.ceil()), and check to see if they are integers (Number.isInteger()).

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Built-in-Objects.js
@@ -0,0 +1,5 @@
+console.log(Math.floor(Math.random() * 100)); // Picks a random whole number between 0 and 100
+
+console.log(Math.ceil(43.8)); // Returns the smallest integer greater than or equal to a decimal number
+
+console.log(Number.isInteger(2017)); // Checks to see if 2017 is an integer
